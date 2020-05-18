9594d7309387b6a0cd932f167b58f9429a40505d
ayoub outiti <outiti.ayoub@gmail.com>
2020-05-18 03:53:38 +0000

    scroll_js

---
+++ public/js/scroll.js
@@ -0,0 +1,17 @@
+const sr = ScrollReveal();
+sr.reveal(".t1", {
+    origin: 'top',
+    distance: "50px",
+    duration: 2000,
+    scale: 0.5,
+    reset: true
+});
+
+
+sr.reveal(".annonce", {
+    origin: 'top',
+    distance: "50px",
+    duration: 2000,
+    scale: 0.5,
+    reset: true
+});
