9bf3cbcbdbda27379c3e9c6dc9d1b6415acb6c5d
ouassimabbari <wassim.abbari@gmail.com>
2020-05-19 06:15:26 +0100

    Les annonces premium s'affiche dans les premiéres pages & page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,23 +1,29 @@
-<?php 
+<?php
 
 namespace App\Controller;
 
 use App\Repository\AdRepository;
+use App\Repository\PremiumRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController extends AbstractController {
+class HomeController extends AbstractController
+{
 
     /**
      * 
      *
      * @Route("/", name = "homepage")
      */
-    function home(AdRepository $adRepo)
+    function home(PremiumRepository $premiumRepo)
     {
+        $premiums = $premiumRepo->findBestAds(3);
+        $ads = array();
+        foreach ($premiums as $premium) {
+            $ads[] = $premium->getAd();
+        }
         return $this->render('home.html.twig', [
-            'ads' => $adRepo->findBestAds(3)
+            'ads' => $ads
         ]);
     }
-
 }
